b4bd187cae272c9c02fb5afa689ad57253f716f7
Richard E. Robbins <RERobbins@iTinker.net>
2023-12-31 09:48:03 -0600

    Primary commit

---
+++ devtools-init.sh
@@ -0,0 +1,39 @@
+build_rer_ml_jupyterlab() {
+    local DATE_TAG=$(date +%Y-%m-%d)
+    docker build \
+	   -t "rer_ml_jupyterlab:$DATE_TAG" \
+	   -t "rer_ml_jupyterlab:latest" \
+	   $HOME/projects/other/devtools
+}
+
+rer_ml_jupyterlab() {
+  if [ -z "$1" ] || [ "$1" = "unstructured" ]; then
+    # Set environment variable to "unstructured"
+    export RER_ML_CONFIG="unstructured"
+    # Launch "unstructured" containers (default)
+    docker-compose -f $HOME/projects/other/devtools/docker-compose.yml up -d
+  elif [ "$1" = "slender" ]; then
+    # Set environment variable to "slender"
+    export RER_ML_CONFIG="slender"
+    # Launch "slender" containers
+    docker-compose -f $HOME/projects/other/devtools/docker-compose-slender.yml up -d
+  else
+    # Display an error message if the argument is not recognized
+    echo "Invalid argument. Usage: rer_ml_jupyterlab [unstructured|slender]"
+  fi
+}
+
+
+
+shutdown_rer_ml_jupyterlab() {
+    if [ -z "$RER_ML_CONFIG" ] || [ "$RER_ML_CONFIG" = "unstructured" ]; then
+	# Shut down "unstructured" containers (default)
+	docker-compose -f $HOME/projects/other/devtools/docker-compose.yml down
+    elif [ "$RER_ML_CONFIG" = "slender" ]; then
+	# Shut down "slender" containers
+	docker-compose -f $HOME/projects/other/devtools/docker-compose-slender.yml down
+    else
+	echo "Invalid configuration stored in RER_ML_CONFIG environment variable."
+    fi
+}
+
