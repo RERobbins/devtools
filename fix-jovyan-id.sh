2e68db11ad139c0acffe770ae9d21ad0c9caa106
Richard Robbins <RERobbins@iTinker.net>
2024-01-08 18:30:11 -0600

    update fix-jovyan-id.sh

--- fix-jovyan-id.sh
+++ fix-jovyan-id.sh
@@ -6,11 +6,11 @@ DEFAULT_GID=100
 
 # Read HOST_UID and HOST_GID from environment with defaults
 HOST_UID=${HOST_UID:-$DEFAULT_UID}
-HOST_GID=${HOST_GID:-$DEFAULT_GID}
+#HOST_GID=${HOST_GID:-$DEFAULT_GID}
 
 # Get current jovyan UID and users GID
 CURRENT_UID=$(id -u jovyan)
-CURRENT_GID=$(id -g jovyan)
+# CURRENT_GID=$(id -g jovyan)
 
 # Update jovyan's UID, if different from HOST_UID
 if [ "$HOST_UID" != "$CURRENT_UID" ]; then
@@ -20,11 +20,11 @@ if [ "$HOST_UID" != "$CURRENT_UID" ]; then
 fi
 
 # Update users GID, if different from HOST_GID
-if [ "$HOST_GID" != "$CURRENT_GID" ]; then
-    groupmod -g $HOST_GID users
+#if [ "$HOST_GID" != "$CURRENT_GID" ]; then
+#    groupmod -g $HOST_GID users
     # Update group ownership in jovyan's home directory
-    chown :$HOST_GID /home/jovyan
-fi
+#    chown :$HOST_GID /home/jovyan
+#fi
 
 # Ensure jovyan is a member of the 'users' group
-usermod -aG users jovyan
+#usermod -aG users jovyan
