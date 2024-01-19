bf540605e24b91542c1d38250b930de0d0d7faa6
Richard Robbins <RERobbins@iTinker.net>
2024-01-19 06:46:28 -0600

    Refine Container Control

--- ml-jupyter/docker-build.sh
+++ ml-jupyter/docker-build.sh
@@ -1,4 +1,4 @@
-build_ml_jupyter () {
+build-ml-jupyter () {
     local DATE_TAG=$(date +%Y-%m-%d)
     local push_to_registry=${1:-true} # Default is true for pushing to the registry
     local TAG_BASE="quay.io/robbins/ml-jupyter"
